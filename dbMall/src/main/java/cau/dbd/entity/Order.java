de612d27552765fd43ef10ddf7154868aa2cdf77
Mason Kim <mskim9967@gmail.com>
2022-12-04 22:32:33 +0900

    Update-order

--- dbMall/src/main/java/cau/dbd/entity/Order.java
+++ dbMall/src/main/java/cau/dbd/entity/Order.java
@@ -1,11 +1,17 @@
 package cau.dbd.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,11 +21,10 @@ import lombok.Setter;
 
 /**
  * 김명승
- * Order은 MYSQL 예약어라 사용 불가능
- * 테이블 명만 Oor
+ * Order은 MYSQL 예약어라 사용 불가능 테이블 명만 Oorder로 수정
  */
 @Entity
-@Table(name="Oorder")
+@Table(name = "Oorder")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -30,8 +35,17 @@ public class Order extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name="consumerId")
+    @JoinColumn(name = "consumerId")
     private Consumer consumer;
+
+//    배송 테이블 추가 시 추가 요망
+//    @ManyToOne
+//    @JoinColumn
+//    private Delivery delivery;
+
+    @OneToMany(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "orderId", updatable = false, insertable = false)
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     @Builder
     public Order(Consumer consumer) {
