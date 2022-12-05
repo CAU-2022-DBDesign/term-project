350138bd19abd7f6a9fffa41b951ac238a72782e
Hyunyeop Ahn <hyeop9@gmail.com>
2022-12-05 18:29:36 +0900

    Delivery Review 추가

---
+++ dbMall/src/main/java/cau/dbd/entity/review/Review.java
@@ -0,0 +1,51 @@
+package cau.dbd.entity.review;
+
+import cau.dbd.entity.BaseTimeEntity;
+import cau.dbd.entity.Consumer;
+import cau.dbd.entity.item.Item;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Review extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "itemId")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "consumerId")
+    private Consumer consumer;
+
+    @Column
+    private String content;
+
+    @Column
+    private int reviewStar;
+
+    @Builder
+    public Review(Item item, Consumer consumer, String content, int reviewStar) {
+        this.item = item;
+        this.consumer = consumer;
+        this.content = content;
+        this.reviewStar = reviewStar;
+    }
+}
